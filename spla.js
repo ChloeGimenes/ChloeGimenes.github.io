b8fe44a404d2af564a449cd9baebeb469af86421
CHLOE GIMENES <chloegimenes@gmail.com>
2021-02-09 15:05:34 +0100

    navbar first steps

---
+++ spla.js
@@ -0,0 +1,19 @@
+
+/* NAVBAR */////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /* RECHERCHE APPEAR-DISAPPEAR *////////////////
+    $(document).ready(function(){     
+
+        $("#b1").click(function(){
+            $("#search-appear").toggle();
+        });
+        
+        // $("#b1").click(function(){
+        //     $("#search-appear").hide();
+        // });
+    });
+
+    function ClearFields() {
+
+        document.getElementById("b2").value = "";
+   }
