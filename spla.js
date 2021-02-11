8589c02f343384680676c5519c203ab07bd8525c
CHLOE GIMENES <chloegimenes@gmail.com>
2021-02-11 19:37:11 +0100

    nav ok

--- spla.js
+++ spla.js
@@ -1,25 +1,37 @@
 
 /* NAVBAR */////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    /* RECHERCHE APPEAR-DISAPPEAR *////////////////
-   
+    // function ShowSearchBar() {
+    //     var x = document.getElementById("search-appear");
+    //     if (x.style.display === "none") {
+    //       x.style.display = "flex";
+    //     } else {
+    //       x.style.display = "none";
+    //     }
+    //   }
 
-    function ShowSearchBar() {
-        var x = document.getElementById("search-appear");
-        if (x.style.display === "none") {
-          x.style.display = "flex";
-        } else {
-          x.style.display = "none";
-        }
+    /* AVOID DROPDOWN MENU CLOSE ON CLICK INSIDE */
+      // $(document).on('click', '.megamenu', function (e) {
+      //   e.stopPropagation(); 
+      // });
+
+
+    /* CLEAR INPUT FIELD ON CLICK BUTTON */
+      function ClearFields() {
+        document.getElementById("b2").value = " ";
       }
 
-    function ClearFields() {
+      // function deleteInput() {
+      //   document.getElementById("b2").value = "";
+      //   }
+      //   document.getElementById("navbar-delete").addEventListener("click", deleteInput);
 
-        document.getElementById("b2").value = "";
-   }
 
-   $('.main-gallery').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
-  });
+
+   /* CAROUSEL /////////////////////////////////////////////////////////////////////////////////////////////*/
+
+      $('.main-gallery').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+      });
