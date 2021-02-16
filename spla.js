e442f6d3b1722aed5947d054ff8cf2301408b18a
CHLOE GIMENES <chloegimenes@gmail.com>
2021-02-16 16:38:22 +0100

    mentions ok

--- spla.js
+++ spla.js
@@ -1,32 +1,7 @@
 
 /* NAVBAR */////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // function ShowSearchBar() {
-    //     var x = document.getElementById("search-appear");
-    //     if (x.style.display === "none") {
-    //       x.style.display = "flex";
-    //     } else {
-    //       x.style.display = "none";
-    //     }
-    //   }
-
-    /* AVOID DROPDOWN MENU CLOSE ON CLICK INSIDE */
-      // $(document).on('click', '.megamenu', function (e) {
-      //   e.stopPropagation(); 
-      // });
-
-  
-  // Ajouter la class active pour les menus
-      $(document).ready(function () {
-      var url = window.location;
-      // Will only work if string in href matches with location
-      $('ul.nav a[href="#"]').parent().addClass('active');
-
-      // Will also work for relative and absolute hrefs
-      $('ul.nav a').filter(function () {
-              return this.href == '#';
-      }).parent().addClass('active').parent().parent().addClass('active');
-      });
+    
 
 
     /* CLEAR INPUT FIELD ON CLICK BUTTON */
